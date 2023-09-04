bb89258d9e75a33c86e3bd8d48e17a1752395b8f
Chandra Sekhar Mullu <47244469+iamknownstranger@users.noreply.github.com>
2023-09-04 17:17:24 +0000

    Update openbb as data source

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,14 @@
+from datetime import date, timedelta
 
 import numpy as np
 import pandas as pd
-import streamlit as st
-from datetime import date, timedelta
-from streamlit_tags import st_tags
 import plotly.express as px
-
-from nsepy import get_history
-from pypfopt.efficient_frontier import EfficientFrontier
-from pypfopt import risk_models
-from pypfopt import expected_returns
+import streamlit as st
+from openbb_terminal.sdk import openbb
+from pypfopt import expected_returns, risk_models
 from pypfopt.discrete_allocation import DiscreteAllocation, get_latest_prices
+from pypfopt.efficient_frontier import EfficientFrontier
+from streamlit_tags import st_tags
 
 st.set_page_config(page_title="Portfolio Analyzer")
 
@@ -22,8 +20,58 @@ with st.form(key='form'):
     symbols = st_tags(
         label='Enter the name or symbol of the stock in your portfolio to analyze',
         text='Press enter to add more',
-        value=["SBIN", "HDFCBANK", "ITC", "ASIANPAINT", "DIVISLAB"],
-        suggestions=['CIPLA', 'BPCL', 'SUNPHARMA', 'JSWSTEEL', 'IOC', 'DRREDDY', 'POWERGRID', 'COALINDIA', 'ITC', 'TITAN', 'DIVISLAB', 'SHREECEM', 'GRASIM', 'ASIANPAINT', 'ONGC', 'BAJAJFINSV', 'SBIN', 'BAJFINANCE', 'HEROMOTOCO', 'SBILIFE', 'KOTAKBANK', 'HDFCBANK', 'ICICIBANK', 'TECHM', 'HCLTECH', 'BAJAJ-AUTO', 'RELIANCE', 'UPL', 'LT', 'INFY', 'HDFC', 'HINDUNILVR', 'EICHERMOT', 'WIPRO', 'TATAMOTORS', 'INDUSINDBK', 'BHARTIARTL', 'TCS', 'AXISBANK', 'ULTRACEMCO', 'HDFCLIFE', 'ADANIPORTS', 'M&M', 'BRITANNIA', 'TATASTEEL', 'NTPC', 'HINDALCO', 'TATACONSUM', 'MARUTI', 'NESTLEIND'])
+        value=["AAPL", "MSFT", "GOOGL", "AMZN", "META"],
+        suggestions = ["MMM", "ABT", "ABBV", "ABMD", "ACN", "ATVI", "ADBE", "AAP", "AMD", "AES",
+            "AFL", "A", "APD", "AKAM", "ALK", "ALB", "ARE", "ALXN", "ALGN", "ALLE",
+            "LNT", "ALL", "GOOGL", "GOOG", "MO", "AMZN", "AMCR", "AEE", "AAL", "AEP",
+            "AXP", "AIG", "AMT", "AWK", "AMP", "ABC", "AME", "AMGN", "APH", "ADI", "ANSS",
+            "ANTM", "AON", "AOS", "APA", "AAPL", "AMAT", "APTV", "ADM", "ANET", "AJG",
+            "AIZ", "T", "ATO", "ADSK", "ADP", "AZO", "AVB", "AVY", "BKR", "BLL", "BAC",
+            "BK", "BAX", "BDX", "BRK.B", "BBY", "BIO", "BIIB", "BLK", "BA", "BKNG",
+            "BWA", "BXP", "BSX", "BMY", "AVGO", "BR", "BF.B", "CHRW", "COG", "CDNS",
+            "CZR", "CPB", "COF", "CAH", "KMX", "CCL", "CARR", "CTLT", "CAT", "CBOE",
+            "CBRE", "CDW", "CE", "CNC", "CNP", "CERN", "CF", "SCHW", "CHTR", "CVX",
+            "CMG", "CB", "CHD", "CI", "CINF", "CTAS", "CSCO", "C", "CFG", "CTXS",
+            "CLX", "CME", "CMS", "KO", "CTSH", "CL", "CMCSA", "CMA", "CAG", "COP",
+            "ED", "STZ", "COO", "CPRT", "GLW", "CTVA", "COST", "CCI", "CSX", "CMI",
+            "CVS", "DHI", "DHR", "DRI", "DVA", "DE", "DAL", "XRAY", "DVN", "DXCM",
+            "FANG", "DLR", "DFS", "DISCA", "DISCK", "DISH", "DG", "DLTR", "D", "DPZ",
+            "DOV", "DOW", "DTE", "DUK", "DRE", "DD", "DXC", "EMN", "ETN", "EBAY",
+            "ECL", "EIX", "EW", "EA", "EMR", "ENPH", "ETR", "EOG", "EFX", "EQIX",
+            "EQR", "ESS", "EL", "ETSY", "EVRG", "ES", "RE", "EXC", "EXPE", "EXPD",
+            "EXR", "XOM", "FFIV", "FB", "FAST", "FRT", "FDX", "FIS", "FITB", "FE",
+            "FRC", "FISV", "FLT", "FMC", "F", "FTNT", "FTV", "FBHS", "FOXA", "FOX",
+            "BEN", "FCX", "GPS", "GRMN", "IT", "GD", "GE", "GIS", "GM", "GPC",
+            "GILD", "GL", "GPN", "GS", "GWW", "HAL", "HBI", "HIG", "HAS", "HCA",
+            "PEAK", "HSIC", "HSY", "HES", "HPE", "HLT", "HFC", "HOLX", "HD", "HON",
+            "HRL", "HST", "HWM", "HPQ", "HUM", "HBAN", "HII", "IEX", "IDXX", "INFO",
+            "ITW", "ILMN", "INCY", "IR", "INTC", "ICE", "IBM", "IP", "IPG", "IFF",
+            "INTU", "ISRG", "IVZ", "IPGP", "IQV", "IRM", "JBHT", "JKHY", "J", "SJM",
+            "JNJ", "JCI", "JPM", "JNPR", "KSU", "K", "KEY", "KEYS", "KMB", "KIM",
+            "KMI", "KLAC", "KHC", "KR", "LHX", "LH", "LRCX", "LW", "LVS", "LEG",
+            "LDOS", "LEN", "LLY", "LNC", "LIN", "LYV", "LKQ", "LMT", "L", "LOW",
+            "LUMN", "LYB", "MTB", "MRO", "MPC", "MKTX", "MAR", "MMC", "MLM", "MAS",
+            "MA", "MKC", "MXIM", "MCD", "MCK", "MDT", "MRK", "MET", "MTD", "MGM",
+            "MCHP", "MU", "MSFT", "MAA", "MHK", "TAP", "MDLZ", "MPWR", "MNST", "MCO",
+            "MS", "MOS", "MSI", "MSCI", "NDAQ", "NTAP", "NFLX", "NWL", "NEM", "NWSA",
+            "NWS", "NEE", "NLSN", "NKE", "NI", "NSC", "NTRS", "NOC", "NLOK", "NCLH",
+            "NOV", "NRG", "NUE", "NVDA", "NVR", "ORLY", "OXY", "ODFL", "OMC", "OKE",
+            "ORCL", "OTIS", "PCAR", "PKG", "PH", "PAYX", "PAYC", "PYPL", "PENN", "PNR",
+            "PBCT", "PEP", "PKI", "PRGO", "PFE", "PM", "PSX", "PNW", "PXD", "PNC",
+            "POOL", "PPG", "PPL", "PFG", "PG", "PGR", "PLD", "PRU", "PTC", "PEG",
+            "PSA", "PHM", "PVH", "QRVO", "PWR", "QCOM", "DGX", "RL", "RJF", "RTX",
+            "O", "REG", "REGN", "RF", "RSG", "RMD", "RHI", "ROK", "ROL", "ROP",
+            "ROST", "RCL", "SPGI", "CRM", "SBAC", "SLB", "STX", "SEE", "SRE", "NOW",
+            "SHW", "SPG", "SWKS", "SNA", "SO", "LUV", "SWK", "SBUX", "STT", "STE",
+            "SYK", "SIVB", "SYF", "SNPS", "SYY", "TMUS", "TROW", "TTWO", "TPR", "TGT",
+            "TEL", "TDY", "TFX", "TER", "TSLA", "TXN", "TXT", "TMO", "TJX", "TSCO",
+            "TT", "TDG", "TRV", "TFC", "TWTR", "TYL", "TSN", "UDR", "ULTA", "USB",
+            "UAA", "UA", "UNP", "UAL", "UNH", "UPS", "URI", "UHS", "UNM", "VFC",
+            "VLO", "VAR", "VTR", "VRSN", "VRSK", "VZ", "VRTX", "VIAC", "V", "VNO",
+            "VMC", "WRB", "WAB", "WMT", "WBA", "DIS", "WM", "WAT", "WEC", "WFC",
+            "WELL", "WST", "WDC", "WU", "WRK", "WY", "WHR", "WMB", "WLTW", "WYNN",
+            "XEL", "XLNX", "XYL", "YUM", "ZBRA", "ZBH", "ZION", "ZTS"]
+            )
 
     start_date_value = date.today() - timedelta(days=365)
     end_date_value = date.today()
@@ -55,11 +103,10 @@ with st.form(key='form'):
 
         df = pd.DataFrame()
         for i in range(len(symbols)):
-            data = get_history(symbol=symbols[i], start=start_date, end=(end_date))[
-                ['Symbol', 'Close']]
+            data = openbb.stocks.load(symbol=symbols[i], start_date=start_date, end_date=end_date)[
+                ['Close']]
             if not data.empty:
-                data.rename(columns={'Close': data['Symbol'][0]}, inplace=True)
-                data.drop(['Symbol'], axis=1, inplace=True)
+                data.rename(columns={'Close': symbols[i]}, inplace=True)
                 if i == 0:
                     df = data
                 if i != 0:
