2fcee152e2ee2648d0ee786cb57536b85aa880b7
Chandra Sekhar Mullu <47244469+iamknownstranger@users.noreply.github.com>
2025-01-25 23:53:32 +0530

    Disable multi_level_index to fix plotting issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -104,13 +104,13 @@ with st.form(key='form'):
 
         df = pd.DataFrame()
         for i in range(len(symbols)):
-            data = yf.download(symbols[i], start=start_date, end=end_date)[['Close']]
+            data = yf.download(symbols[i], start=start_date, end=end_date, multi_level_index=False)[['Close']]
             if not data.empty:
                 data = data.rename(columns={'Close': symbols[i]})
-                if i == 0:
+                if df.empty:
                     df = data
-                if i != 0:
-                    df = df.join(data)
+                else:
+                    df = pd.concat([df, data], axis=1)
             else:
                 st.warning(f"Invalid stock symbol: {symbols[i]}")
 
